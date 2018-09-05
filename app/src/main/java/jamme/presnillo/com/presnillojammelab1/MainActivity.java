41866204ff8707e173841190627c0e95e9827ea4
jammlouise <2015081366@ust-ics.mygbiz.com>
2018-09-05 17:57:51 +0800

    Initial commit

---
+++ app/src/main/java/jamme/presnillo/com/presnillojammelab1/MainActivity.java
@@ -0,0 +1,15 @@
+package jamme.presnillo.com.presnillojammelab1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        //setContentView(R.layout.relative_layout);
+        setContentView(R.layout.linear_layout);
+    }
+}
